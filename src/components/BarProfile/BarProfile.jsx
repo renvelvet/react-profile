c28f738d23626dc19cd1b3c9005a4b3032abc0a6
Resha Puspita <reshapuspita734@gmail.com>
2020-07-01 12:17:17 +0700

    bar profile done

---
+++ src/components/BarProfile/BarProfile.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./BarProfile.css";
+import avatar from "../../assets/ava.jpg";
+function BarProfile() {
+  return (
+    <div className="bar-profile">
+      <div className="block-ava">
+        <img src={avatar} alt="avatar" />
+      </div>
+      <div className="block-data">
+        <h3>RESHA PUSPITA DEWI</h3>
+        <h4>BOOTCAMP STUDENT</h4>
+        <h4>
+          "<em>Motto ceritanya</em>"
+        </h4>
+      </div>
+    </div>
+  );
+}
+
+export default BarProfile;
---
+++ src/pages/Profile.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Profile.css";
+import BarProfile from "../components/BarProfile/BarProfile";
+function Profile() {
+  return (
+    <div>
+      <BarProfile />
+    </div>
+  );
+}
+
+export default Profile;
